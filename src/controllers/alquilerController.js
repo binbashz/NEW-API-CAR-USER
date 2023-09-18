336d69c9c463afe01d9e9194895cf0a9ffdff542
binbashz <5732@holbertonstudents.com>
2023-09-18 20:58:02 -0300

    control alq auto-tarifa

--- src/controllers/alquilerController.js
+++ src/controllers/alquilerController.js
@@ -5,12 +5,11 @@ exports.alquilarAuto = async (req, res) => {
     const { autoId, fechaInicio, fechaFinal } = req.body;
   
     try {
-      // Aquí debes calcular el costo del alquiler en función de la tarifa diaria y la duración en días
-      // Puedes utilizar la fórmula que mencioné en una respuesta anterior o cualquier otra lógica que prefieras.
+      //  calcular el costo del alquiler en función de la tarifa diaria y la duración en días
+     
+// Después de calcular el costo, almacenar los detalles del alquiler en tu base de datos si es necesario.
   
-      // Después de calcular el costo, puedes almacenar los detalles del alquiler en tu base de datos si es necesario.
-  
-      // Responder con un mensaje de éxito y el costo total
+// Responder con un mensaje de éxito y el costo total
       res.status(200).json({ mensaje: 'Alquiler exitoso', costoTotal: costoCalculado });
     } catch (error) {
       console.error(error);
